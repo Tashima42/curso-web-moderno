1f3155ecfc5f4d8678dcb492a0e0db25c5c8e1eb
Pedro Tashima <23709916+Tashima42@users.noreply.github.com>
2020-07-14 14:57:56 -0300

    Exercício 8

---
+++ ex8.js
@@ -0,0 +1,25 @@
+/*
+08) Pedro joga N jogos de basquete por temporada. Para saber como está ele está progredindo, ele mantém
+registro de todos os as pontuações feitas por jogo. Após cada jogo ele anota no novo valor e confere se o
+mesmo é maior ou menor que seu melhor e pior desempenho. Dada uma lista string = “pontuação1 pontuação2
+pontuação3 etc..”, escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
+vetor com o número de vezes que ele bateu seu recorde de maior número de pontos e quando fez seu pior
+jogo. (Número do pior jogo).
+*/
+let pontuacoes = []
+
+function addPontuacao(pontuacao) {
+    pontuacoes.push(pontuacao)
+
+    let maiorPontuacao = pontuacao
+
+    pontuacoes.forEach( (item, index) => {
+        if(maiorPontuacao < pontuacoes) maiorPontuacao = pontuacoes[index] 
+    })
+    console.log(maiorPontuacao)
+}
+
+addPontuacao("200")
+addPontuacao("900")
+addPontuacao("100")
+addPontuacao("2000")
