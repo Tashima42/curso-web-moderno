be0d27e5eb657a73fa742bc796ef9e5c0b64d040
Pedro Tashima <pedrotashima42@gmail.com>
2020-07-16 09:34:28 -0300

    Exercício 23

--- exercicios-js/ex23.js
+++ exercicios-js/ex23.js
@@ -6,12 +6,36 @@ Mostre o código do aluno, suas três notas, a média calculada e uma mensagem "
 */
 
 function mediaPonderadaAluno(codigo, nota1, nota2, nota3) {
-    let notas = [nota1, nota2, nota3]
-    let maiorNota = 0
-    const pesoMaiorNota = 4
-    const pesoOutrasNotas = 3
-    
-    notas.forEach((item, index) => {
-        if(notas[index] > maiorNota) maiorNota = notas[index]
-    })
+    while (codigo > 1) {
+        const notas = [nota1, nota2, nota3]
+        let notasComPeso = []
+        let maiorNota = 0
+        let mediaPonderadaFinal
+        let resultadoFinal
+
+        notas.forEach((item, index) => {
+            if (notas[index] > maiorNota) {
+                maiorNota = notas[index]
+            }
+        })
+
+        for (let i = 0; i < notas.length; i++) {
+            if (notas[i] == maiorNota) {
+                notasComPeso[i] = notas[i] * 4
+            } else {
+                notasComPeso[i] = notas[i] * 3
+            }
+        }
+
+
+        mediaPonderadaFinal = (notasComPeso[0] + notasComPeso[1] + notasComPeso[2]) / 10
+        if (mediaPonderadaFinal >= 5) resultadoFinal = "APROVADO"
+        else resultadoFinal = "REPROVADO"
+
+
+        return `Aluno: ${codigo} - Nota 1: ${notas[0]}, Nota 2: ${notas[1]}, Nota 3: ${notas[2]} - Média: ${mediaPonderadaFinal} | ${resultadoFinal}`
+    }
 }
+
+console.log(mediaPonderadaAluno(2, 3, 4, 1))
+console.log(mediaPonderadaAluno(90, 7, 6, 5))
