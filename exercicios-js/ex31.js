a476f81da53e5dd22da22b9e3b85ad6f994b222d
Pedro Tashima <pedrotashima42@gmail.com>
2020-07-16 18:51:28 -0300

    Exercício 31

--- exercicios-js/ex31.js
+++ exercicios-js/ex31.js
@@ -1,3 +1,12 @@
 /*
-31)​Escrever um algoritmo que percorre um vetor de inteiros, conta quantos números negativos há nesse vetore imprime a quantidade no console.
+31)​Escrever um algoritmo que percorre um vetor de inteiros, conta quantos números negativos há nesse vetor e imprime a quantidade no console.
 */
+
+const vetorInteiros = [2,4,6,-1,2,-5,-3,-2,12,-890,-12,-42,1,5,7,23,-12]
+let quantidadeNegativos = 0
+
+vetorInteiros.forEach((item, index) => {
+    if (vetorInteiros[index] < 0) quantidadeNegativos++ 
+})
+
+console.log(quantidadeNegativos)
---
+++ exercicios-js/ex32.js
@@ -0,0 +1,3 @@
+/*
+32)​ Construir um algoritmo que calcule a média aritmética dos valores de um vetor de inteiros.
+*/
