b7dfb8f51c902793f7648aee14afda7326094ef7
Pedro Tashima <pedrotashima42@gmail.com>
2020-07-15 23:22:07 -0300

    Create ex23.js

---
+++ exercicios-js/ex23.js
@@ -0,0 +1,17 @@
+/*
+23)​ Escreva um algoritmo que leia o código de um aluno e suas três notas. 
+Calcule a média ponderada do aluno, considerando que o peso para a maior nota seja 4 e para as duas restantes, 3. 
+Mostre o código do aluno, suas três notas, a média calculada e uma mensagem "APROVADO" se a média for maior ou igual a 5 e 
+"REPROVADO" se a média for menor que 5. Repita a operação até que o código lido seja negativo.
+*/
+
+function mediaPonderadaAluno(codigo, nota1, nota2, nota3) {
+    let notas = [nota1, nota2, nota3]
+    let maiorNota = 0
+    const pesoMaiorNota = 4
+    const pesoOutrasNotas = 3
+    
+    notas.forEach((item, index) => {
+        if(notas[index] > maiorNota) maiorNota = notas[index]
+    })
+}
