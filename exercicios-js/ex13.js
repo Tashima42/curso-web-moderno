c02073d142a81fa92d314ef7b55e02d22652c565
Pedro Tashima <pedrotashima42@gmail.com>
2020-07-16 23:59:14 -0300

    Consertado Exercício 20

--- exercicios-js/ex13.js
+++ exercicios-js/ex13.js
@@ -1,16 +1,22 @@
 /*
-13)​Crie um programa que exibe se um dia é dia útil, fim de semana ou dia inválido dado o número referente aodia. Considere que domingo é o dia 1 e sábado é o dia 7. Utilize a estrutura Switch.
+13)​Crie um programa que exibe se um dia é dia útil, fim de semana ou dia inválido dado o número referente ao
+dia. Considere que domingo é o dia 1 e sábado é o dia 7. Utilize a estrutura Switch.
 */
-//TODO: Exercício 13
-
 function diaUtil(dia){
     switch (dia) {
-        case ((dia % 7) == 0):
-            return `fim de semana`
-        case (2):
-            return `Dia útil`
+        case 1:
+        case 7:
+            return `Final de semana`;break
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            return `Dia útil`;break
+        default: return `Dia inválido`;break
     }
 }
 
-
 console.log(diaUtil(7))
+console.log(diaUtil(2))
+console.log(diaUtil(19))
--- exercicios-js/ex20.js
+++ exercicios-js/ex20.js
@@ -4,9 +4,34 @@ considerando notas de R$ 100, R$ 50, R$ 10 e R$ 5 e R$ 1.Seu programa deve mostr
 Por exemplo, ao solicitar R$18, o programa deve informar apenas a seguinte informação 
 (note que não foram exibidas informações sobre as demais cédulas): 1 nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1.
 */
-//TODO: Exercício 20
-function trocoCedulas(valor){
-    
+function trocoCedulas(valor) {
+    const notas = [100, 50, 10, 5, 1]
+    let notas100 = 0, notas50 = 0, notas10 = 0, notas5 = 0, notas1 = 0
+
+    while (valor > 0) {
+        while (valor > 100) {
+            valor -= 100
+            notas100++
+        }
+        while (valor > 50) {
+            valor -= 50
+            notas50++
+        }
+        while (valor > 10) {
+            valor -= 10
+            notas10++
+        }
+        while (valor > 5) {
+            valor -= 5
+            notas5++
+        }
+        while (valor >= 1) {
+            valor -= 1
+            notas1++
+        }
+    }
+
+    return `${notas1} nota(s) de R$1, ${notas5} nota(s) de R$5, ${notas10} nota(s) de R$10, ${notas50} nota(s) de R$50, ${notas100} nota(s) de R$100`
 }
 
-console.log(trocoCedulas(89))
+console.log(trocoCedulas(289))
